a5d5aa7f86c514a0025e25ce19e583caa21091e0
Sebastián Muñoz <sebastian@Sebastians-MacBook-Air.local>
2022-08-25 23:10:28 -0500

    Add model validations.

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,2 +1,7 @@
 class Sample < ApplicationRecord
+    has_one_attached :text_sample
+    validates :id, uniqueness: true
+    validates :name, presence: true, format: {with: /\A[0-9a-zA-Z]*\z/}
+    validates :words_count, numericality: { only_integer: true }
+    validates :cycles, numericality: { only_integer: true }
 end
